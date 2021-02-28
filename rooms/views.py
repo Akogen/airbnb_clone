6e4103c4af664ce76e2d2097c72d3a7af35b4db6
Jason Jung <akogenweb@gmail.com>
2021-02-27 22:08:25 -0800

    10.Introduction_to_Views_and_URLs

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,27 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse  # for sample2 only
+from . import models
 
-# Create your views here.
+# without render sample
+def sample2(request):
+    now = datetime.now()
+    return HttpResponse(content="<h1>Hello {}</h1>".format(now))
+
+
+# with render sample
+def sample(request):
+    now = datetime.now()
+    hungry = True
+    return render(request, "sample.html", context={"now": now, "hungry": hungry})
+
+
+def home(request):
+    all_rooms = models.Room.objects.all()
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+        },
+    )
