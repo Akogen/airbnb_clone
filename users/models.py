2d5fb58af25e83784d08e5364d00e91827326421
Jason Jung <akogenweb@gmail.com>
2021-03-08 21:22:56 -0800

    14.Log_in_out

--- users/models.py
+++ users/models.py
@@ -30,6 +30,10 @@ class User(AbstractUser):
     )
     bio = models.TextField(default="")
     birthdate = models.DateField(blank=True, null=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_ENGLISH
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_ENGLISH
+    )
     super_hosts = models.BooleanField(default=False)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from django.views import View
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
-# Create your views here.
+
+class LoginView_scracth(View):
+
+    """ LoginView Definition """
+
+    def get(self, request):
+
+        form = forms.LoginForm()
+
+        return render(request, "users/login.html", {"form": form})
+
+    def post(self, request):
+
+        form = forms.LoginForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+
+            user = authenticate(username=email, password=password)
+
+            if user is not None:
+
+                # A backend authenticated the credential
+                login(request, user)
+                return redirect(reverse("core:home"))
+
+        return render(request, "users/login.html", {"form": form})
+
+
+class LoginView(FormView):
+
+    """ LoginView Definition """
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    # initial = {"email": "email@email.com"}
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+
+        user = authenticate(self.request, username=email, password=password)
+
+        if user is not None:
+
+            # A backend authenticated the credential
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+
+    logout(request)
+
+    return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    """ SignUp View Definition"""
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+    initial = {"first_name": "David", "last_name": "Cool", "email": "email@email.com"}
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+
+        user = authenticate(self.request, username=email, password=password)
+
+        if user is not None:
+
+            # A backend authenticated the credential
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def logn_view(request):
+
+    if request.method == "GET":
+        pass
+
+    elif request.method == "P":
+        pass
