6958a9e6af2f2dfe327cacb0945aecddfd9352ac
Jason Jung <akogenweb@gmail.com>
2021-03-09 23:24:42 -0800

    16.Verify_Email

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
-from . import forms
+from . import forms, models
 
 
 class LoginView_scracth(View):
@@ -84,13 +84,20 @@ class SignUpView(FormView):
 
             # A backend authenticated the credential
             login(self.request, user)
+
+        user.verify_email()
+
         return super().form_valid(form)
 
 
-def logn_view(request):
-
-    if request.method == "GET":
+def complete_verification(request, key):
+    try:
+        user = models.User.objects.get(email_secret=key)
+        user.email_verified = True
+        user.email_secret = ""
+        user.save()
+        # to do: add succes message
+    except models.User.DoesNotExist:
+        # to do: add error message
         pass
-
-    elif request.method == "P":
-        pass
+    return redirect(reverse("core:home"))
